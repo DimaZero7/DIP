119f819956bc5763124ab1960c33c300cf9a5e70
Алексей Тендеткин <56300722+iSpeedMan@users.noreply.github.com>
2020-06-04 23:44:20 +0300

    fix1

--- authorization/views.py
+++ authorization/views.py
@@ -2,7 +2,7 @@ from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
-
+from django.shortcuts import  HttpResponse
 
 
 class ChangePassword(SuccessMessageMixin, PasswordResetView):
@@ -23,10 +23,15 @@ class Logout(SuccessMessageMixin, LogoutView):
     
 class Registration(SuccessMessageMixin, FormView):
     form_class = UserCreationForm
-    success_url = "/"
+    success_url = "/authorization/login/"
     template_name = "authorization/register.html"
+    
     
     def form_valid(self, form):
         form.save()
         return super(Registration, self).form_valid(form)
-
+    
+#    def form_invalid(self, form):
+#        response = super(SuccessMessageMixin, self).form_invalid(form)
+#        success_message = "Исправте ошибки"
+#        return HttpResponse()
