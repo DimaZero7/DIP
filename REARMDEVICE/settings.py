a9fb333c61231ce1299ebd5b83f85aa545ce45db
Алексей Тендеткин <56300722+iSpeedMan@users.noreply.github.com>
2020-06-07 20:35:53 +0300

    deploy

--- REARMDEVICE/settings.py
+++ REARMDEVICE/settings.py
@@ -140,17 +140,6 @@ USE_TZ = True
 
 
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = '<YOUR BUCKET NAME>'
-
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/
-
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
