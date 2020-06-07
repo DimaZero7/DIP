8abe1af041ea98ee8e2436bfcfba155437f9a70b
Алексей Тендеткин <56300722+iSpeedMan@users.noreply.github.com>
2020-06-07 17:19:42 +0300

    Развертывание4

--- REARMDEVICE/settings.py
+++ REARMDEVICE/settings.py
@@ -155,7 +155,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_URL = 'http://rearmd.s3.amazonaws.com/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
